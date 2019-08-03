144a1cfb165d8bf78bd7fe74723fa028f8cae216
Chicho25 <pedroarrieta25@hotmail.com>
2019-08-02 21:38:18 -0500

    presentacion

---
+++ present.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pedro Arrieta - Programador</title>
+    <style media="screen">
+    img{
+      display:block;
+      margin:auto;
+      }
+    </style>
+  </head>
+  <body>
+    <img src="images/present.jpg" alt="">
+  </body>
+</html>
